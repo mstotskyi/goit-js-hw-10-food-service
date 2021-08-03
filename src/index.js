fd1c9aeb933bc7f34cf041f3109d8e8b560803a3
Mykola Stotskyi <stotskyi.m@gmail.com>
2021-08-03 23:36:21 +0300

    змінює тему

--- src/index.js
+++ src/index.js
@@ -9,27 +9,36 @@ const Theme = {
 
 const themeSwitcher = document.querySelector('#theme-switch-toggle');
 
-
-
-
-
+checkTheme ()
 
 themeSwitcher.addEventListener("change", theme);
 
+function checkTheme () {
 
+    let savedTheme = localStorage.getItem('bodyTheme')
+ console.log(savedTheme);
+if ( savedTheme !== null){
+    document.body.classList.add(savedTheme);
+    if(savedTheme === Theme.DARK)
+    {themeSwitcher.checked = true};
+    }
+else {
+    document.body.classList.add(Theme.LIGHT);
+    themeSwitcher.checked = false;
+    console.log(savedTheme);
+}
+}
 
 function theme (evt) {
     console.log(evt.target.checked);
     if (evt.target.checked){
         console.log(`темна тема`);
-        localStorage.setItem('bodyTheme', "dark")
-        document.body.classList.add(Theme.DARK);
+        document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+        localStorage.setItem('bodyTheme', Theme.DARK)
             }
     else{
-         console.log(`світла тема`);
-         localStorage.setItem('bodyTheme', "light")
-         document.body.classList.replace(Theme.DARK, Theme.LIGHT)
-    
-}
-    
+        console.log(`світла тема`);
+        document.body.classList.replace(Theme.DARK, Theme.LIGHT)
+        localStorage.setItem('bodyTheme', Theme.LIGHT)
+    }
 }
