f030ca4c3cbb7045979ca152d978f8789e889b56
Mykola Stotskyi <stotskyi.m@gmail.com>
2021-08-03 18:44:36 +0300

    змінює тему

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './sass/main.scss';
+
+
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+const themeSwitcher = document.querySelector('#theme-switch-toggle');
+
+
+
+
+
+
+themeSwitcher.addEventListener("change", theme);
+
+
+
+function theme (evt) {
+    console.log(evt.target.checked);
+    if (evt.target.checked){
+        console.log(`темна тема`);
+        localStorage.setItem('bodyTheme', "dark")
+        document.body.classList.add(Theme.DARK);
+            }
+    else{
+         console.log(`світла тема`);
+         localStorage.setItem('bodyTheme', "light")
+         document.body.classList.replace(Theme.DARK, Theme.LIGHT)
+    
+}
+    
+}
