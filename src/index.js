98a070d25f1648fc5d20f1b5316d5057f61bfd15
Mykola Stotskyi <stotskyi.m@gmail.com>
2021-08-04 21:00:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,8 +17,7 @@ themeSwitcher.addEventListener("change", theme);
 function checkTheme () {
 
     let savedTheme = localStorage.getItem('bodyTheme')
- console.log(savedTheme);
-if ( savedTheme !== null){
+ if ( savedTheme !== null){
     document.body.classList.add(savedTheme);
     if(savedTheme === Theme.DARK)
     {themeSwitcher.checked = true};
@@ -26,19 +25,15 @@ if ( savedTheme !== null){
 else {
     document.body.classList.add(Theme.LIGHT);
     themeSwitcher.checked = false;
-    console.log(savedTheme);
-}
+    }
 }
 
 function theme (evt) {
-    console.log(evt.target.checked);
     if (evt.target.checked){
-        console.log(`темна тема`);
         document.body.classList.replace(Theme.LIGHT, Theme.DARK);
         localStorage.setItem('bodyTheme', Theme.DARK)
             }
     else{
-        console.log(`світла тема`);
         document.body.classList.replace(Theme.DARK, Theme.LIGHT)
         localStorage.setItem('bodyTheme', Theme.LIGHT)
     }
